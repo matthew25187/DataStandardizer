6e87ae3a485bac07bc683d8fc1e29fa401028339
matthew25187 <maintainer@datastandardizer.com>
2025-08-22 22:30:13 +1200

    feat: Add DataStandardizer.Chronology project and tests - Introduced a new `DataStandardizer.Chronology` project for handling time zone data based on the TZ Database. - Added `TzDataTimezone` struct and `TzDataTimezoneAttribute` for representing time zones and their metadata. - Implemented extension methods in `TzDataExtensions` for retrieving time zone comments, ISO country codes, latitude, and longitude. - Created unit tests in `DataStandardizer.Chronology.Tests` to validate the functionality of `TzDataExtensions`. - Updated solution and pipeline configurations to include the new project and its tests. AB#4293

---
+++ src/DataStandardizer.Chronology/TzDataExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+#if NETSTANDARD
+using JetBrains.Annotations; 
+#endif
+
+namespace DataStandardizer.Chronology
+{
+    public static class TzDataExtensions
+    {
+        /// <summary>
+        /// Get the comment on a timezone.
+        /// </summary>
+        /// <param name="timezone">A TzData timezone.</param>
+        /// <returns>Comment for the timezone, if available; otherwise, <c>null</c>.</returns>
+#if NETCOREAPP3_0_OR_GREATER
+        public static string? GetComment(this TzDataTimezone timezone)
+#else
+        [CanBeNull]
+        public static string GetComment(this TzDataTimezone timezone)
+#endif
+        {
+            var timezoneAttribute = GetTimezoneAttribute(timezone);
+            return timezoneAttribute?.Comment;
+        }
+
+        /// <summary>
+        /// Get the ISO country codes for the countries covered by a timezone.
+        /// </summary>
+        /// <param name="timezone">A TzData timezone.</param>
+        /// <returns>A collection of ISO country codes related to the timezone.</returns>
+        public static string[] GetIsoCountryCodes(this TzDataTimezone timezone)
+        {
+            var timezoneAttribute = GetTimezoneAttribute(timezone);
+#if NETSTANDARD1_3_OR_GREATER
+            return timezoneAttribute?.IsoCountryCodes ?? Array.Empty<string>();
+#else
+            return timezoneAttribute?.IsoCountryCodes ?? new string[] { };
+#endif
+        }
+
+        /// <summary>
+        /// Get the latitude of the principal location within a timezone.
+        /// </summary>
+        /// <param name="timezone">A TzData timezone.</param>
+        /// <returns>Latitude of the timezone.</returns>
+        public static double GetLatitude(this TzDataTimezone timezone)
+        {
+            var timezoneAttribute = GetTimezoneAttribute(timezone);
+            return timezoneAttribute?.Latitude ?? 0;
+        }
+
+        /// <summary>
+        /// Get the longitude of the principal location within a timezone.
+        /// </summary>
+        /// <param name="timezone">A TzData timezone.</param>
+        /// <returns>Longitude of the timezone.</returns>
+        public static double GetLongitude(this TzDataTimezone timezone)
+        {
+            var timezoneAttribute = GetTimezoneAttribute(timezone);
+            return timezoneAttribute?.Longitude ?? 0;
+        }
+
+#if NETCOREAPP3_0_OR_GREATER
+        private static TzDataTimezoneAttribute? GetTimezoneAttribute(TzDataTimezone timezone)
+#else
+        [CanBeNull]
+        private static TzDataTimezoneAttribute GetTimezoneAttribute(TzDataTimezone timezone)
+#endif
+        {
+            var timezoneFields = GetTimezoneFields(timezone.GetType());
+            var timezoneField = timezoneFields.FirstOrDefault(field => field.GetValue(null) is TzDataTimezone fieldValue && fieldValue.Equals(timezone));
+            return timezoneField?.GetCustomAttribute<TzDataTimezoneAttribute>();
+        }
+
+        private static FieldInfo[] GetTimezoneFields(Type hostType)
+        {
+            var timezoneFields = hostType.GetTypeInfo().DeclaredFields
+                .Where(field => field.FieldType == typeof(TzDataTimezone))
+                .ToList();
+
+            var nestedTypes = hostType.GetTypeInfo().DeclaredNestedTypes;
+            foreach (var nestedType in nestedTypes)
+            {
+                var nestedTimezoneFields = GetTimezoneFields(nestedType.AsType());
+                timezoneFields.AddRange(nestedTimezoneFields);
+            }
+
+            return timezoneFields.ToArray();
+        }
+    }
+}
