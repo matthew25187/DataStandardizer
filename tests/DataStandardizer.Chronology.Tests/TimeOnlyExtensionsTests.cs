91b4c77fb9ffbfb35093a5fca4b447c2b7c50391
Build Service <buildservice@example.com>
2025-11-23 23:12:49 +1300

    Enhance TimeOnlyExtensionsTests with Additional Test Cases for DOS Date/Time Conversion - Expanded test coverage in `TimeOnlyExtensionsTests` for `ToDosDateTime` method. - Added multiple `InlineData` test cases to validate conversion of various `TimeOnly` values to DOS date/time format. - Improved code formatting and readability in test methods. AB#4351

--- tests/DataStandardizer.Chronology.Tests/TimeOnlyExtensionsTests.cs
+++ tests/DataStandardizer.Chronology.Tests/TimeOnlyExtensionsTests.cs
@@ -5,14 +5,20 @@ namespace DataStandardizer.Chronology.Tests;
 public class TimeOnlyExtensionsTests
 {
     [Theory]
-    [InlineData(0,0,0,0)]
+    [InlineData(0, 0, 0, 0x210000),
+     InlineData(0, 0, 2, 0x210001),
+     InlineData(6, 15, 30, 0x2131EF),
+     InlineData(12, 30, 0, 0x2163C0),
+     InlineData(15, 45, 10, 0x217DA5),
+     InlineData(23, 59, 58, 0x21bf7d)]
     public void ToDosDateTime_OnTimeOnlyValue_ReturnsDosDateTimeEquivalent(ushort testHour, ushort testMinute, ushort testSecond, uint expectedResult)
-    {// arrange
+    {
+        // arrange
         var testTime = new TimeOnly(testHour, testMinute, testSecond);
-        
+
         // act
         var testResult = testTime.ToDosDateTime();
-        
+
         // assert
         ((uint)testResult).Should().Be(expectedResult);
     }
