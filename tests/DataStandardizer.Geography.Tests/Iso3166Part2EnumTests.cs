960211c9e1ae8b00f9e5cbc6c85576ac1cd74bee
matthew25187 <maintainer@datastandardizer.com>
2025-12-20 22:13:43 +1300

    Enhanced test coverage for ISO 3166-2 subdivision retrieval methods - Added new test cases in `Iso3166Part2EnumTests`:   - `GetNames_InvalidIso3166Part1Alpha2Country_ReturnsEmptySet`   - `GetNames_InvalidIso3166Part1Alpha3Country_ReturnsEmptySet`   - `GetNames_Iso3166Part1Alpha2CountryNoSubdivisions_ReturnsEmptySet`   - `GetNames_Iso3166Part1Alpha3CountryNoSubdivisions_ReturnsEmptySet`   - `GetValues_InvalidIso3166Part1Alpha2Country_ReturnsEmptySet`   - `GetValues_InvalidIso3166Part1Alpha3Country_ReturnsEmptySet`   - `GetValues_Iso3166Part1Alpha2CountryNoSubdivisions_ReturnsEmptySet`   - `GetValues_Iso3166Part1Alpha3CountryNoSubdivisions_ReturnsEmptySet` - Improved validation for scenarios with invalid or unsupported country codes. - Ensured empty sets are returned for countries without subdivisions. These changes enhance the robustness and reliability of the ISO 3166-2 subdivision retrieval functionality. AB#4363

--- tests/DataStandardizer.Geography.Tests/Iso3166Part2EnumTests.cs
+++ tests/DataStandardizer.Geography.Tests/Iso3166Part2EnumTests.cs
@@ -5,6 +5,16 @@ namespace DataStandardizer.Geography.Tests;
 
 public class Iso3166Part2EnumTests
 {
+    [Fact]
+    public void GetNames_InvalidIso3166Part1Alpha2Country_ReturnsEmptySet()
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetNames(default(Iso3166Part1Alpha2Country));
+
+        // assert
+        testResult.Should().BeEmpty();
+    }
+
     [Theory]
     [ClassData(typeof(GetNames_Iso3166Part1Alpha2Country_TestCaseFactory))]
     public void GetNames_Iso3166Part1Alpha2Country_ReturnsNamesOfSubdivisionFields(Iso3166Part1Alpha2Country testValue, string[] expectedResult)
@@ -46,6 +56,36 @@ public class Iso3166Part2EnumTests
         {
             return ((IEnumerable)_testCases).GetEnumerator();
         }
+    }
+
+    [Theory]
+    [InlineData(Iso3166Part1Alpha2Country.AI), InlineData(Iso3166Part1Alpha2Country.AQ), InlineData(Iso3166Part1Alpha2Country.AS), InlineData(Iso3166Part1Alpha2Country.AW), InlineData(Iso3166Part1Alpha2Country.AX),
+     InlineData(Iso3166Part1Alpha2Country.BL), InlineData(Iso3166Part1Alpha2Country.BM), InlineData(Iso3166Part1Alpha2Country.BV), InlineData(Iso3166Part1Alpha2Country.CC), InlineData(Iso3166Part1Alpha2Country.CK),
+     InlineData(Iso3166Part1Alpha2Country.CW), InlineData(Iso3166Part1Alpha2Country.CX), InlineData(Iso3166Part1Alpha2Country.EH), InlineData(Iso3166Part1Alpha2Country.FK), InlineData(Iso3166Part1Alpha2Country.FO),
+     InlineData(Iso3166Part1Alpha2Country.GF), InlineData(Iso3166Part1Alpha2Country.GG), InlineData(Iso3166Part1Alpha2Country.GI), InlineData(Iso3166Part1Alpha2Country.GP), InlineData(Iso3166Part1Alpha2Country.GS),
+     InlineData(Iso3166Part1Alpha2Country.GU), InlineData(Iso3166Part1Alpha2Country.HK), InlineData(Iso3166Part1Alpha2Country.HM), InlineData(Iso3166Part1Alpha2Country.IM), InlineData(Iso3166Part1Alpha2Country.IO),
+     InlineData(Iso3166Part1Alpha2Country.JE), InlineData(Iso3166Part1Alpha2Country.KY), InlineData(Iso3166Part1Alpha2Country.MF), InlineData(Iso3166Part1Alpha2Country.MO), InlineData(Iso3166Part1Alpha2Country.MP),
+     InlineData(Iso3166Part1Alpha2Country.MQ), InlineData(Iso3166Part1Alpha2Country.MS), InlineData(Iso3166Part1Alpha2Country.NC), InlineData(Iso3166Part1Alpha2Country.NF), InlineData(Iso3166Part1Alpha2Country.NU),
+     InlineData(Iso3166Part1Alpha2Country.PF), InlineData(Iso3166Part1Alpha2Country.PM), InlineData(Iso3166Part1Alpha2Country.PN), InlineData(Iso3166Part1Alpha2Country.PR), InlineData(Iso3166Part1Alpha2Country.RE),
+     InlineData(Iso3166Part1Alpha2Country.SJ), InlineData(Iso3166Part1Alpha2Country.SX), InlineData(Iso3166Part1Alpha2Country.TC), InlineData(Iso3166Part1Alpha2Country.TF), InlineData(Iso3166Part1Alpha2Country.TK),
+     InlineData(Iso3166Part1Alpha2Country.VA), InlineData(Iso3166Part1Alpha2Country.VG), InlineData(Iso3166Part1Alpha2Country.VI), InlineData(Iso3166Part1Alpha2Country.YT)]
+    public void GetNames_Iso3166Part1Alpha2CountryNoSubdivisions_ReturnsEmptySet(Iso3166Part1Alpha2Country testValue)
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetNames(testValue);
+
+        // assert
+        testResult.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetNames_InvalidIso3166Part1Alpha3Country_ReturnsEmptySet()
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetNames(default(Iso3166Part1Alpha3Country));
+        
+        // assert
+        testResult.Should().BeEmpty();
     }
 
     [Theory]
@@ -92,6 +132,36 @@ public class Iso3166Part2EnumTests
     }
 
     [Theory]
+    [InlineData(Iso3166Part1Alpha3Country.AIA), InlineData(Iso3166Part1Alpha3Country.ATA), InlineData(Iso3166Part1Alpha3Country.ASM), InlineData(Iso3166Part1Alpha3Country.ABW), InlineData(Iso3166Part1Alpha3Country.ALA),
+     InlineData(Iso3166Part1Alpha3Country.BLM), InlineData(Iso3166Part1Alpha3Country.BMU), InlineData(Iso3166Part1Alpha3Country.BVT), InlineData(Iso3166Part1Alpha3Country.CCK), InlineData(Iso3166Part1Alpha3Country.COK),
+     InlineData(Iso3166Part1Alpha3Country.CUW), InlineData(Iso3166Part1Alpha3Country.CXR), InlineData(Iso3166Part1Alpha3Country.ESH), InlineData(Iso3166Part1Alpha3Country.FLK), InlineData(Iso3166Part1Alpha3Country.FRO),
+     InlineData(Iso3166Part1Alpha3Country.GUF), InlineData(Iso3166Part1Alpha3Country.GGY), InlineData(Iso3166Part1Alpha3Country.GIB), InlineData(Iso3166Part1Alpha3Country.GLP), InlineData(Iso3166Part1Alpha3Country.SGS),
+     InlineData(Iso3166Part1Alpha3Country.GUM), InlineData(Iso3166Part1Alpha3Country.HKG), InlineData(Iso3166Part1Alpha3Country.HMD), InlineData(Iso3166Part1Alpha3Country.IMN), InlineData(Iso3166Part1Alpha3Country.IOT),
+     InlineData(Iso3166Part1Alpha3Country.JEY), InlineData(Iso3166Part1Alpha3Country.CYM), InlineData(Iso3166Part1Alpha3Country.MAF), InlineData(Iso3166Part1Alpha3Country.MAC), InlineData(Iso3166Part1Alpha3Country.MNP),
+     InlineData(Iso3166Part1Alpha3Country.MTQ), InlineData(Iso3166Part1Alpha3Country.MSR), InlineData(Iso3166Part1Alpha3Country.NCL), InlineData(Iso3166Part1Alpha3Country.NFK), InlineData(Iso3166Part1Alpha3Country.NIU),
+     InlineData(Iso3166Part1Alpha3Country.PYF), InlineData(Iso3166Part1Alpha3Country.SPM), InlineData(Iso3166Part1Alpha3Country.PCN), InlineData(Iso3166Part1Alpha3Country.PRI), InlineData(Iso3166Part1Alpha3Country.REU),
+     InlineData(Iso3166Part1Alpha3Country.SJM), InlineData(Iso3166Part1Alpha3Country.SXM), InlineData(Iso3166Part1Alpha3Country.TCA), InlineData(Iso3166Part1Alpha3Country.ATF), InlineData(Iso3166Part1Alpha3Country.TKL),
+     InlineData(Iso3166Part1Alpha3Country.VAT), InlineData(Iso3166Part1Alpha3Country.VGB), InlineData(Iso3166Part1Alpha3Country.VIR), InlineData(Iso3166Part1Alpha3Country.MYT)]
+    public void GetNames_Iso3166Part1Alpha3CountryNoSubdivisions_ReturnsEmptySet(Iso3166Part1Alpha3Country testValue)
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetNames(testValue);
+
+        // assert
+        testResult.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetValues_InvalidIso3166Part1Alpha2Country_ReturnsEmptySet()
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetValues(default(Iso3166Part1Alpha2Country));
+
+        // assert
+        testResult.Should().BeEmpty();
+    }
+
+    [Theory]
     [ClassData(typeof(GetValues_Iso3166Part1Alpha2Country_TestCaseFactory))]
     public void GetValues_Iso3166Part1Alpha2Country_ReturnsValuesOfSubdivisionFields(Iso3166Part1Alpha2Country testValue, Iso3166Part2Subdivision[] expectedResult)
     {
@@ -135,6 +205,36 @@ public class Iso3166Part2EnumTests
     }
 
     [Theory]
+    [InlineData(Iso3166Part1Alpha2Country.AI), InlineData(Iso3166Part1Alpha2Country.AQ), InlineData(Iso3166Part1Alpha2Country.AS), InlineData(Iso3166Part1Alpha2Country.AW), InlineData(Iso3166Part1Alpha2Country.AX),
+     InlineData(Iso3166Part1Alpha2Country.BL), InlineData(Iso3166Part1Alpha2Country.BM), InlineData(Iso3166Part1Alpha2Country.BV), InlineData(Iso3166Part1Alpha2Country.CC), InlineData(Iso3166Part1Alpha2Country.CK),
+     InlineData(Iso3166Part1Alpha2Country.CW), InlineData(Iso3166Part1Alpha2Country.CX), InlineData(Iso3166Part1Alpha2Country.EH), InlineData(Iso3166Part1Alpha2Country.FK), InlineData(Iso3166Part1Alpha2Country.FO),
+     InlineData(Iso3166Part1Alpha2Country.GF), InlineData(Iso3166Part1Alpha2Country.GG), InlineData(Iso3166Part1Alpha2Country.GI), InlineData(Iso3166Part1Alpha2Country.GP), InlineData(Iso3166Part1Alpha2Country.GS),
+     InlineData(Iso3166Part1Alpha2Country.GU), InlineData(Iso3166Part1Alpha2Country.HK), InlineData(Iso3166Part1Alpha2Country.HM), InlineData(Iso3166Part1Alpha2Country.IM), InlineData(Iso3166Part1Alpha2Country.IO),
+     InlineData(Iso3166Part1Alpha2Country.JE), InlineData(Iso3166Part1Alpha2Country.KY), InlineData(Iso3166Part1Alpha2Country.MF), InlineData(Iso3166Part1Alpha2Country.MO), InlineData(Iso3166Part1Alpha2Country.MP),
+     InlineData(Iso3166Part1Alpha2Country.MQ), InlineData(Iso3166Part1Alpha2Country.MS), InlineData(Iso3166Part1Alpha2Country.NC), InlineData(Iso3166Part1Alpha2Country.NF), InlineData(Iso3166Part1Alpha2Country.NU),
+     InlineData(Iso3166Part1Alpha2Country.PF), InlineData(Iso3166Part1Alpha2Country.PM), InlineData(Iso3166Part1Alpha2Country.PN), InlineData(Iso3166Part1Alpha2Country.PR), InlineData(Iso3166Part1Alpha2Country.RE),
+     InlineData(Iso3166Part1Alpha2Country.SJ), InlineData(Iso3166Part1Alpha2Country.SX), InlineData(Iso3166Part1Alpha2Country.TC), InlineData(Iso3166Part1Alpha2Country.TF), InlineData(Iso3166Part1Alpha2Country.TK),
+     InlineData(Iso3166Part1Alpha2Country.VA), InlineData(Iso3166Part1Alpha2Country.VG), InlineData(Iso3166Part1Alpha2Country.VI), InlineData(Iso3166Part1Alpha2Country.YT)]
+    public void GetValues_Iso3166Part1Alpha2CountryNoSubdivisions_ReturnsEmptySet(Iso3166Part1Alpha2Country testValue)
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetValues(testValue);
+
+        // assert
+        testResult.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetValues_InvalidIso3166Part1Alpha3Country_ReturnsEmptySet()
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetValues(default(Iso3166Part1Alpha3Country));
+
+        // assert
+        testResult.Should().BeEmpty();
+    }
+
+    [Theory]
     [ClassData(typeof(GetValues_Iso3166Part1Alpha3Country_TestCaseFactory))]
     public void GetValues_Iso3166Part1Alpha3Country_ReturnsValuesOfSubdivisionFields(Iso3166Part1Alpha3Country testValue, Iso3166Part2Subdivision[] expectedResult)
     {
@@ -175,5 +275,25 @@ public class Iso3166Part2EnumTests
         {
             return ((IEnumerable)_testCases).GetEnumerator();
         }
+    }
+
+    [Theory]
+    [InlineData(Iso3166Part1Alpha3Country.AIA), InlineData(Iso3166Part1Alpha3Country.ATA), InlineData(Iso3166Part1Alpha3Country.ASM), InlineData(Iso3166Part1Alpha3Country.ABW), InlineData(Iso3166Part1Alpha3Country.ALA),
+     InlineData(Iso3166Part1Alpha3Country.BLM), InlineData(Iso3166Part1Alpha3Country.BMU), InlineData(Iso3166Part1Alpha3Country.BVT), InlineData(Iso3166Part1Alpha3Country.CCK), InlineData(Iso3166Part1Alpha3Country.COK),
+     InlineData(Iso3166Part1Alpha3Country.CUW), InlineData(Iso3166Part1Alpha3Country.CXR), InlineData(Iso3166Part1Alpha3Country.ESH), InlineData(Iso3166Part1Alpha3Country.FLK), InlineData(Iso3166Part1Alpha3Country.FRO),
+     InlineData(Iso3166Part1Alpha3Country.GUF), InlineData(Iso3166Part1Alpha3Country.GGY), InlineData(Iso3166Part1Alpha3Country.GIB), InlineData(Iso3166Part1Alpha3Country.GLP), InlineData(Iso3166Part1Alpha3Country.SGS),
+     InlineData(Iso3166Part1Alpha3Country.GUM), InlineData(Iso3166Part1Alpha3Country.HKG), InlineData(Iso3166Part1Alpha3Country.HMD), InlineData(Iso3166Part1Alpha3Country.IMN), InlineData(Iso3166Part1Alpha3Country.IOT),
+     InlineData(Iso3166Part1Alpha3Country.JEY), InlineData(Iso3166Part1Alpha3Country.CYM), InlineData(Iso3166Part1Alpha3Country.MAF), InlineData(Iso3166Part1Alpha3Country.MAC), InlineData(Iso3166Part1Alpha3Country.MNP),
+     InlineData(Iso3166Part1Alpha3Country.MTQ), InlineData(Iso3166Part1Alpha3Country.MSR), InlineData(Iso3166Part1Alpha3Country.NCL), InlineData(Iso3166Part1Alpha3Country.NFK), InlineData(Iso3166Part1Alpha3Country.NIU),
+     InlineData(Iso3166Part1Alpha3Country.PYF), InlineData(Iso3166Part1Alpha3Country.SPM), InlineData(Iso3166Part1Alpha3Country.PCN), InlineData(Iso3166Part1Alpha3Country.PRI), InlineData(Iso3166Part1Alpha3Country.REU),
+     InlineData(Iso3166Part1Alpha3Country.SJM), InlineData(Iso3166Part1Alpha3Country.SXM), InlineData(Iso3166Part1Alpha3Country.TCA), InlineData(Iso3166Part1Alpha3Country.ATF), InlineData(Iso3166Part1Alpha3Country.TKL),
+     InlineData(Iso3166Part1Alpha3Country.VAT), InlineData(Iso3166Part1Alpha3Country.VGB), InlineData(Iso3166Part1Alpha3Country.VIR), InlineData(Iso3166Part1Alpha3Country.MYT)]
+    public void GetValues_Iso3166Part1Alpha3CountryNoSubdivisions_ReturnsEmptySet(Iso3166Part1Alpha3Country testValue)
+    {
+        // act
+        var testResult = Iso3166Part2Enum.GetValues(testValue);
+
+        // assert
+        testResult.Should().BeEmpty();
     }
 }
