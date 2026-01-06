7a06c11780066a7e86b6a55c63e5cc570ad2a576
matthew25187 <maintainer@datastandardizer.com>
2026-01-06 21:09:56 +1300

    Refactor and enhance Azure DevOps pipeline templates and scripts - Introduced modular pipeline templates for build, QA, and publication stages:   - Added `build-apply-project-version.yml` for applying version numbers to projects.   - Added `build-steps.yml` for build-related tasks.   - Added `qa-steps.yml` for QA and testing tasks.   - Added `publication-create-packages-prepare.yml` and `publication-publish-packages-job.yml` for package preparation and publishing. - Replaced inline scripts with reusable PowerShell scripts:   - Added `ConfirmCurrentVersionNumber.ps1` for verifying current version numbers.   - Added `ConfirmVersionNumberRollback.ps1` for validating version rollbacks.   - Added `UpdatePipelineVersionNumber.ps1` for updating pipeline version numbers. - Refactored `datastandardizer-ci-build-pipeline.yml`:   - Replaced redundant inline steps with references to modular templates.   - Renamed parameters for clarity (e.g., `requirePackageDataStandardizer*` to `forcePackageDataStandardizer*`).   - Simplified conditional logic for package actions. - Removed deprecated script `ReserveProductionVersionNumbers.ps1`. - Improved maintainability and readability of pipeline configurations and scripts. AB#4372

---
+++ build/scripts/ConfirmVersionNumberRollback.ps1
@@ -0,0 +1,59 @@
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]    $PackageName,
+    
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]    $PackageInfos,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]    $EncodedPackageVersions,
+
+    [Parameter()]
+    [int]    $TraceLevel = 0
+)
+
+$modulePath = Join-Path $PSScriptRoot "../psmodules/CommonHelpers/CommonHelpers.psm1"
+Import-Module $modulePath -Force
+
+if (0 -lt $TraceLevel) {
+    Set-PSDebug -Trace $TraceLevel
+}
+
+# set environment variable for current process
+$env:AZURE_DEVOPS_EXT_PAT = $env:SYSTEM_ACCESSTOKEN
+                                                      
+az devops configure -d organization=${env:ORGANIZATION_URL} project=${env:PROJECT_NAME}
+az account set -s ${env:SUBSCRIPTION_ID}
+
+# Get package information.
+$packageInfo = $PackageInfos | ConvertFrom-Json | Where-Object -Property packageName -EQ -Value $PackageName
+if ($null -eq $packageInfo) {
+    Write-Error "Package information not found for $PackageName."
+}
+
+$packageVersions = $EncodedPackageVersions | ConvertFrom-Base64String | ConvertFrom-Json | Where-Object -Property PackageName -EQ -Value '${{packageName}}'
+if ($null -eq $packageVersions) {
+    Write-Error "Package versions not found for $PackageName."
+}
+[version]$nextPackageVersion = $packageVersions.PackageNextVersion
+
+# Fetch next package version from pipeline.
+$variableListOutput = & az pipelines variable-group variable list --group-id $packageInfo.variableGroupId
+$variableGroupVersionNumbers = ($variableListOutput | ConvertFrom-Json).PSObject.Properties |
+Where-Object { $_.Name.StartsWith('next') } |
+Out-String -InputObject { $_.Name + ':' + $_.Value.value } -Stream |
+Sort-Object |
+ConvertFrom-Csv -Delimiter ':' -Header 'Name', 'Value' |
+Out-String -InputObject { $_.Value } -Stream
+[version] $currentPackageVersion = ($variableGroupVersionNumbers[0], $variableGroupVersionNumbers[1], $variableGroupVersionNumbers[2], $variableGroupVersionNumbers[3] -join '.')
+
+# Verify rollback was successful.
+if ($currentPackageVersion -ne $nextPackageVersion) {
+    Write-Error "Failed to rollback package version for $PackageName to $nextPackageVersion; actual version is $currentPackageVersion."
+}
+                                
+[version]$oldPackageVersion = '$(currentNextMajor).$(currentNextMinor).$(currentNextPatch).$(currentNextPreview)'
+Write-Information "Rolled back package $($packageInfo.packageName) next version from v$oldPackageVersion to v$currentPackageVersion." -InformationAction Continue
---
+++ build/scripts/UpdatePipelineVersionNumber.ps1
@@ -0,0 +1,47 @@
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]    $PackageName,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]    $VersionNumberName,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]    $VariableNamePrefix,
+
+    [Parameter()]
+    [int]    $TraceLevel = 0
+)
+
+$modulePath = Join-Path $PSScriptRoot "../psmodules/CommonHelpers/CommonHelpers.psm1"
+Import-Module $modulePath -Force
+
+if (0 -lt $TraceLevel) {
+    Set-PSDebug -Trace $TraceLevel
+}
+
+# set environment variable for current process
+$env:AZURE_DEVOPS_EXT_PAT = $env:SYSTEM_ACCESSTOKEN
+                      
+az devops configure -d organization=${env:ORGANIZATION_URL} project=${env:PROJECT_NAME}
+az account set -s ${env:SUBSCRIPTION_ID}
+
+# Extract package metadata.
+$packageInfo = ($env:PACKAGEINFOS | ConvertFrom-Json) | Where-Object -Property packageName -EQ -Value $PackageName
+if ($null -eq $packageInfo) {
+    Write-Error "Package information not found for $PackageName."
+}
+
+$packageVersions = ($env:VERSIONNUMBERS | ConvertFrom-Base64String | ConvertFrom-Json) | Where-Object -Property PackageName -EQ -Value $PackageName
+if ($null -eq $packageVersions) {
+    Write-Error "Package versions not found for $PackageName."
+}
+
+# Update version numbers in pipeline.
+[version]$updateVersionNumber = Invoke-Expression ('$packageInfo.' + $VersionNumberName)
+az pipelines variable-group variable update --group-id $packageInfo.variableGroupId --name "$VariableNamePrefix-major-number" --value $updateVersionNumber.Major --verbose
+az pipelines variable-group variable update --group-id $packageInfo.variableGroupId --name "$VariableNamePrefix-minor-number" --value $updateVersionNumber.Minor --verbose
+az pipelines variable-group variable update --group-id $packageInfo.variableGroupId --name "$VariableNamePrefix-patch-number" --value $updateVersionNumber.Build --verbose
+az pipelines variable-group variable update --group-id $packageInfo.variableGroupId --name "$VariableNamePrefix-preview-number" --value $updateVersionNumber.Revision --verbose
