3710a942486999b957d210ad6fa69fc0fe7a74b3
matthew25187 <maintainer@datastandardizer.com>
2026-01-08 23:26:25 +1300

    Add confirmation preference to `DeclareRequiredVersionNumbers.ps1`. - Set `$ConfirmPreference` to `'None'` to suppress confirmation prompts during module installation. - Ensured smooth installation of the `ConvertFrom-StringTable` module from the PowerShell Gallery. AB#4373

--- build/scripts/DeclareRequiredVersionNumbers.ps1
+++ build/scripts/DeclareRequiredVersionNumbers.ps1
@@ -19,6 +19,7 @@ param (
 )
 
 # Install from PowerShell Gallery
+$ConfirmPreference = 'None'
 Install-Module -Name ConvertFrom-StringTable -Scope CurrentUser
 
 # Import the module into your session
